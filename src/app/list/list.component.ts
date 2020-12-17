e70774a835fcca955d58aa3fd374fb6766ddc995
tdingsun <tiger.dingsun@gmail.com>
2020-12-16 23:17:45 -0500

    colors

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { List } from '../list';
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -8,11 +8,20 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
   @Input() list: List;
   hover: boolean = false;
   constructor(private router:Router, private route:ActivatedRoute) { }
 
+  ngOnInit(): void {
+    if (this.list.color == "#979143"){ // color hack for tiger
+      this.list.color = "darkkhaki";
+    }
+    if (this.list.color == "#6fa96f"){ // color hack for maria
+      this.list.color = "darkseagreen";
+    }
+  }
+  
   onClick(name): void {
     this.router.navigate([`/${name}`]);
   }
